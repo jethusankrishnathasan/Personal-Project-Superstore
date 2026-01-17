e88d328d6f703693feb968b4908630961542e85b
jethusankrishnathasan <jethusan1@gmail.com>
2026-01-17 22:59:26 +0000

    Add files via upload

---
+++ Sql/Load_Data.sql
@@ -0,0 +1,7 @@
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Superstore_Cleaned.csv'
+INTO TABLE data
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
